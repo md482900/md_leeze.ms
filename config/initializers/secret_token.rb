2beae579a38c35f5e9de7be9d51423e525487e4b
Manh Thang Do <manhthangd@yahoo.de>
2012-11-30 13:00:47 +0100

    push dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeezeMs::Application.config.secret_token = 'f0398f509a7d5c0effee350c5c39982b59897b378b00db8b3ac41f58e6f179b049789e04409b96b557683dd0c9f15d3c6769708c3506c4dba63814395f419bf8'
